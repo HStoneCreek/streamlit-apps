19055e446466ffea4947a6a2abc2f218133888fb
Hendrik Steinbach <hendriksteinbach@Hendriks-MBP.fritz.box>
2020-10-09 19:07:48 +0200

    Anpassungen Text Sentiment

--- textSentiment.py
+++ textSentiment.py
@@ -16,7 +16,7 @@ import numpy as np
 from matplotlib.patches import Circle, Wedge, Rectangle
 st.set_option('deprecation.showPyplotGlobalUse', False)
 import spacy
-import en_core_web_lg
+#import en_core_web_lg
 import de_core_news_lg
 nlpEN = en_core_web_lg.load()
 nlpDE = de_core_news_lg.load()
